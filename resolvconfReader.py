a281a4b6a25eb53991e083a9f75766e9ef99a13a
Don Jajo <donjajo4all@gmail.com>
2017-04-03 11:45:24 +0100

    Escape commented lines

--- resolvconfReader.py
+++ resolvconfReader.py
@@ -4,6 +4,8 @@ def get_resolvers():
 	try:
 		with open( '/etc/resolv.conf', 'r' ) as resolvconf:
 			for line in resolvconf.readlines():
+				line = line.split( '#', 1 )[ 0 ];
+				line = line.rstrip();
 				if 'nameserver' in line:
 					resolvers.append( line.split()[ 1 ] )
 		return resolvers
